1553c342e1973c65ddb0ff8298d54e85b328e72d
Niki <43398321+niki-cy@users.noreply.github.com>
2018-12-19 14:22:15 +0900

    Update desktop.js

--- src/js/desktop.js
+++ src/js/desktop.js
@@ -13,14 +13,18 @@
             var VIDEO_LINK = config.select_link_field;
             var VIDEO_WIDTH = config.insert_width;
             var VIDEO_HEIGHT = config.insert_height;
+            var VIDEO_BLANK_SPACE = config.select_space_field;
 
             var fullUrl = resp.record[VIDEO_LINK].value;
             if (fullUrl.slice(0,32) === 'https://www.youtube.com/watch?v=') {
                 var videoId = fullUrl.slice(32);
             } else if (fullUrl.slice(0,17) === 'https://youtu.be/') {
                 var videoId = fullUrl.slice(17);
-            };
+            } else {
+                alert('The Youtube URL could not be read.\nInsert the URL either as https://www.youtube.com/watch?v=OOOOOOOOOOO\nor https://youtu.be/OOOOOOOOOOO.')
+            }
 
+            // Create iFrame HTML element
             var ytiframe = document.createElement('iframe');
             ytiframe.setAttribute('width', VIDEO_WIDTH);
             ytiframe.setAttribute('height', VIDEO_HEIGHT);
@@ -29,7 +33,7 @@
             ytiframe.setAttribute('allow', 'accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture');
             ytiframe.setAttribute('allowfullscreen', 'allowfullscreen');
 
-            kintone.app.record.getSpaceElement('Video').appendChild(ytiframe);
+            kintone.app.record.getSpaceElement(VIDEO_BLANK_SPACE).appendChild(ytiframe);
 
         }, function(error) {
             console.log(error);
