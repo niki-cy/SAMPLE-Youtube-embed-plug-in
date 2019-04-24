f3741037dfc871f065333d1013b2e9090a6a0046
Niki <43398321+niki-cy@users.noreply.github.com>
2019-04-24 13:10:59 +0900

    Update config.js

--- src/js/config.js
+++ src/js/config.js
@@ -9,12 +9,12 @@ jQuery.noConflict();
   var $space = $('#select_space_field'); // Drop-down with ID 'select_space_field'
   var $width = $('#insert_width'); // Drop-down with ID 'select_link_field'
   var $height = $('#insert_height'); // Drop-down with ID 'select_link_field'
+  var $ratio = $('#ratio_checkbox'); // Checkbox with ID 'ratio_checkbox'
   var appId = kintone.app.getId(); // Variable with the App ID
-
-  // Set body for API requests
-  var body = {
-    'app': appId
-  };
+  var checkbox_boolean; // Last saved value of checkbox changed back from string to boolean
+  var checkbox_value; // Last saved value of the checkbox (string-version boolean)
+  var newWidth; // Ratio-correct width when height is changed and aspect ratio lock enabled
+  var newHeight; // Ratio-correct height when width is changed and aspect ratio lock enabled
 
   // Retrieve URL to App Settings page
   function getSettingsUrl() {
@@ -23,32 +23,20 @@ jQuery.noConflict();
 
   // Retrieve Link field information, then set drop-down
   function setLinkDropdown() {
-
-    return kintone.api(kintone.api.url('/k/v1/preview/app/form/fields', true), 'GET', body, function(resp) {
-      // Success
-      var props = resp.properties;
-      var field;
-      var linkField;
+    KintoneConfigHelper.getFields(['SINGLE_LINE_TEXT', 'LINK']).then(function(resp) {
       var $optionLink;
 
-      for (linkField in props) {
-        if (props.hasOwnProperty(linkField)) {
-          field = props[linkField];
-          // Allow both Link fields set as URL and Text fields
-          if (field.protocol === 'WEB' || field.type === 'SINGLE_LINE_TEXT') {
-            $optionLink = $('<option>');
-            $optionLink.attr('value', field.code);
-            $optionLink.text(field.label);
-            $link.append($optionLink);
-          }
-        }
+      for (var i = 0; i < resp.length; i++) {
+        $optionLink = $('<option>');
+        $optionLink.attr('value', resp[i].code);
+        $optionLink.text(resp[i].label);
+        $link.append($optionLink);
       }
 
       if (config.select_link_field) {
         $link.val(config.select_link_field);
       }
-
-    }, function(error) {
+    }).catch(function(err) {
       // Error
       alert('There was an error retrieving the Link field information.');
     });
@@ -56,36 +44,23 @@ jQuery.noConflict();
 
   // Retrieve Blank Space field information, then set drop-down
   function setSpaceDropdown() {
-
-    return kintone.api(kintone.api.url('/k/v1/preview/app/form/layout', true), 'GET', body, function(resp) {
-      // Success
-      var i;
-      var j;
-      var rowFields;
-      var fieldType;
-      var spaceId;
+    KintoneConfigHelper.getFields('SPACER').then(function(resp) {
       var $optionSpace;
+      var spaceId;
 
-      for (i = 0; i < resp.layout.length; i++) {
-        rowFields = resp.layout[i].fields;
-
-        for (j = 0; j < rowFields.length; j++) {
-          fieldType = rowFields[j].type;
-          if (fieldType === 'SPACER') {
-            spaceId = rowFields[j].elementId;
-            $optionSpace = $('<option>');
-            $optionSpace.attr('value', spaceId);
-            $optionSpace.text(spaceId);
-            $space.append($optionSpace);
-          }
-        }
-
-        if (config.select_space_field) {
-          $space.val(config.select_space_field);
-        }
+      for (var i = 0; i < resp.length; i++) {
+        spaceId = resp[i].elementId;
+        $optionSpace = $('<option>');
+        $optionSpace.attr('value', spaceId);
+        $optionSpace.text(spaceId);
+        $space.append($optionSpace);
       }
 
-    }, function(error) {
+      if (config.select_space_field) {
+        $space.val(config.select_space_field);
+      }
+
+    }).catch(function(err) {
       // Error
       alert('There was an error retrieving the Blank Space field information.');
     });
@@ -100,6 +75,38 @@ jQuery.noConflict();
     $height.val(config.insert_height);
   }
 
+  // Set saved checkbox value
+  if (config.checkbox_status === 'true') {
+    checkbox_boolean = true;
+  } else {
+    checkbox_boolean = false;
+  }
+
+  $ratio.prop('checked', checkbox_boolean);
+
+  // Auto-change width or height to match 16:9 ratio if aspect ratio check box is checked
+  $width.change(function() {
+    if ($ratio.prop('checked')) {
+      newWidth = Number($width.val());
+      newHeight = newWidth * (9 / 16);
+      $height.val(Math.round(newHeight));
+      checkbox_value = 'true';
+    } else {
+      checkbox_value = 'false';
+    }
+  });
+
+  $height.change(function() {
+    if ($ratio.prop('checked')) {
+      newHeight = Number($height.val());
+      newWidth = newHeight * (16 / 9);
+      $width.val(Math.round(newWidth));
+      checkbox_value = 'true';
+    } else {
+      checkbox_value = 'false';
+    }
+  });
+
   // Run setting functions
   setLinkDropdown();
   setSpaceDropdown();
@@ -111,7 +118,13 @@ jQuery.noConflict();
 
   // Set input values when save button is clicked
   $('#settings-save').click(function(e) {
-    var configSettings = {select_link_field: $link.val(), select_space_field: $space.val(), insert_width: $width.val(), insert_height: $height.val()};
+    var configSettings = {
+      select_link_field: $link.val(),
+      select_space_field: $space.val(),
+      insert_width: $width.val(),
+      insert_height: $height.val(),
+      checkbox_status: checkbox_value
+    };
     var widthNum = Number($width.val());
     var heightNum = Number($height.val());
     var maxDimension = 99999;
@@ -122,7 +135,7 @@ jQuery.noConflict();
       alert('Please fill in all required fields.');
     } else if (!Number.isInteger(widthNum) || !Number.isInteger(heightNum) || widthNum > maxDimension || heightNum > maxDimension) {
       // Check that width and height are full numbers less than 5000
-      alert('Please enter width and height as full numbers less than 99999px');
+      alert('Please enter width and height as full numbers equal to or less than 99999px');
     } else {
       kintone.plugin.app.setConfig(configSettings, function() {
         // Redirect to App Settings
