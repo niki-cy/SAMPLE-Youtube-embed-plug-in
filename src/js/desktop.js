e5d31251b596281bbbbbc603529f00a0a77422a8
Niki <43398321+niki-cy@users.noreply.github.com>
2019-04-24 13:11:14 +0900

    Update desktop.js

--- src/js/desktop.js
+++ src/js/desktop.js
@@ -2,32 +2,17 @@
   'use strict';
   // Record details event
   kintone.events.on('app.record.detail.show', function(event) {
-
-    var body = {
-      'app': kintone.app.getId(),
-      'id': kintone.app.record.getId()
-    };
-
-    kintone.api(kintone.api.url('/k/v1/record', true), 'GET', body, function(resp) {
-      var config = kintone.plugin.app.getConfig(PLUGIN_ID); // Get plug-in setting configuration
-      var VIDEO_LINK = config.select_link_field;
-      var VIDEO_WIDTH = config.insert_width;
-      var VIDEO_HEIGHT = config.insert_height;
-      var VIDEO_BLANK_SPACE = config.select_space_field;
-      var videoId = '';
-      var ytiframe = document.createElement('iframe');
-      var fullUrl = resp.record[VIDEO_LINK].value;
-      var urlExampleFull = 'https://www.youtube.com/watch?v=OOOOOOOOOOO';
-      var urlExampleShort = 'https://youtu.be/OOOOOOOOOOO';
-
-      if (fullUrl.slice(0, 32) === 'https://www.youtube.com/watch?v=') {
-        videoId = fullUrl.slice(32);
-      } else if (fullUrl.slice(0, 17) === 'https://youtu.be/') {
-        videoId = fullUrl.slice(17);
-      } else {
-        alert('The Youtube URL could not be read.\n Insert the URL either as ' + urlExampleFull + '\nor ' + urlExampleShort + '.');
-      }
-
+    var config = kintone.plugin.app.getConfig(PLUGIN_ID); // Get plug-in setting configuration
+    var VIDEO_LINK = config.select_link_field;
+    var VIDEO_WIDTH = config.insert_width;
+    var VIDEO_HEIGHT = config.insert_height;
+    var VIDEO_BLANK_SPACE = config.select_space_field;
+    var videoId = '';
+    var ytiframe = document.createElement('iframe');
+    var fullUrl = event.record[VIDEO_LINK].value;
+    var urlExampleFull = 'https://www.youtube.com/watch?v=OOOOOOOOOOO';
+    var urlExampleShort = 'https://youtu.be/OOOOOOOOOOO';
+    var createYTiframe = function() {
       // Create iFrame HTML element
       ytiframe.setAttribute('width', VIDEO_WIDTH);
       ytiframe.setAttribute('height', VIDEO_HEIGHT);
@@ -35,10 +20,23 @@
       ytiframe.setAttribute('frameborder', '0');
       ytiframe.setAttribute('allow', 'accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture');
       ytiframe.setAttribute('allowfullscreen', 'allowfullscreen');
-
       kintone.app.record.getSpaceElement(VIDEO_BLANK_SPACE).appendChild(ytiframe);
+    };
+    var createNoVideoError = function() {
+      // Create no video error message
+      var noVideoError = document.createElement('div');
+      noVideoError.innerHTML = 'The Youtube URL could not be read.\n Insert the URL either as ' + urlExampleFull + '\nor ' + urlExampleShort + '.';
+      kintone.app.record.getSpaceElement(VIDEO_BLANK_SPACE).appendChild(noVideoError);
+    };
 
-    }, function(error) {
-    });
+    if (fullUrl.slice(0, 32) === 'https://www.youtube.com/watch?v=') {
+      videoId = fullUrl.slice(32);
+      createYTiframe();
+    } else if (fullUrl.slice(0, 17) === 'https://youtu.be/') {
+      videoId = fullUrl.slice(17);
+      createYTiframe();
+    } else {
+      createNoVideoError();
+    }
   });
 })(kintone.$PLUGIN_ID);
