7b19cbb674609351c99a748215ea38ba9436a99b
Niki <43398321+niki-cy@users.noreply.github.com>
2019-08-06 16:30:09 +0900

    Update config.js

--- src/js/config.js
+++ src/js/config.js
@@ -4,150 +4,142 @@ jQuery.noConflict();
   'use strict';
 
   // Get configuration settings
-  var config = kintone.plugin.app.getConfig(PLUGIN_ID);
-  var $link = $('#select_link_field'); // Drop-down with ID 'select_link_field'
-  var $space = $('#select_space_field'); // Drop-down with ID 'select_space_field'
-  var $width = $('#insert_width'); // Drop-down with ID 'select_link_field'
-  var $height = $('#insert_height'); // Drop-down with ID 'select_link_field'
-  var $ratio = $('#ratio_checkbox'); // Checkbox with ID 'ratio_checkbox'
-  var appId = kintone.app.getId(); // Variable with the App ID
+  var CONF = kintone.plugin.app.getConfig(PLUGIN_ID);
+  var $form = $('.js-submit-settings');
+  var $cancelButton = $('.js-cancel-button');
+  var $link = $('select[name="js-select-link-field"]');
+  var $space = $('select[name="js-select-space-field"]');
+  var $width = $('input[name="js-insert-width"]');
+  var $height = $('input[name="js-insert-height"]');
+  var $ratio = $('input[name="js-ratio-enabled"]'); // Checkbox with ID 'ratio_checkbox'
+  var $reset = $('#js-reset-dimensions'); // Button to reset default ratios
   var checkbox_boolean; // Last saved value of checkbox changed back from string to boolean
   var checkbox_value = 'true'; // Last saved value of the checkbox (string-version boolean)
   var newWidth; // Ratio-correct width when height is changed and aspect ratio lock enabled
   var newHeight; // Ratio-correct height when width is changed and aspect ratio lock enabled
 
-  // Retrieve URL to App Settings page
-  function getSettingsUrl() {
-    return '/k/admin/app/flow?app=' + appId;
-  }
-
   // Retrieve Link field information, then set drop-down
-  function setLinkDropdown() {
-    KintoneConfigHelper.getFields(['SINGLE_LINE_TEXT', 'LINK']).then(function(resp) {
-      var $optionLink;
+  function setDropDown() {
+    KintoneConfigHelper.getFields(['SINGLE_LINE_TEXT', 'LINK', 'SPACER']).then(function(resp) {
+      var $linkDropDown = $link;
+      var $spaceDropDown = $space;
 
       resp.forEach(function(respField) {
-        $optionLink = $('<option>');
-        $optionLink.attr('value', respField.code);
-        $optionLink.text(respField.label);
-        $link.append($optionLink);
+        var $option = $('<option></option>');
+        switch (respField.type) {
+          case 'SINGLE_LINE_TEXT':
+          case 'LINK':
+            $option.attr('value', respField.code);
+            $option.text(respField.label);
+            $linkDropDown.append($option.clone());
+            break;
+          case 'SPACER':
+            if (!respField.elementId) {
+              break;
+            }
+            $option.attr('value', respField.elementId);
+            $option.text(respField.elementId);
+            $spaceDropDown.append($option.clone());
+            break;
+          default:
+            break;
+        }
       });
 
-      if (config.select_link_field) {
-        $link.val(config.select_link_field);
+      // Set default values
+      if (CONF.link) {
+        $linkDropDown.val(CONF.link);
       }
-    }).catch(function(err) {
+      if (CONF.space) {
+        $spaceDropDown.val(CONF.space);
+      }
+    }, function(resp) {
       // Error
-      alert('There was an error retrieving the Link field information.');
+      return alert('There was an error retrieving the Link field information.');
     });
   }
 
-  // Retrieve Blank Space field information, then set drop-down
-  function setSpaceDropdown() {
-    KintoneConfigHelper.getFields('SPACER').then(function(resp) {
-      var $optionSpace;
-      var spaceId;
+  $(document).ready(function() {
 
-      resp.forEach(function(respSpace) {
-        spaceId = respSpace.elementId;
-        $optionSpace = $('<option>');
-        $optionSpace.attr('value', spaceId);
-        $optionSpace.text(spaceId);
-        $space.append($optionSpace);
-      });
+    // Set saved width and height values
+    if (CONF.width) {
+      $width.val(CONF.width);
+    }
 
-      if (config.select_space_field) {
-        $space.val(config.select_space_field);
+    if (CONF.height) {
+      $height.val(CONF.height);
+    }
+
+    // Set saved checkbox value
+    if (CONF.checkbox === 'true') {
+      checkbox_boolean = true;
+    } else {
+      checkbox_boolean = false;
+    }
+
+    $ratio.prop('checked', checkbox_boolean);
+
+    // Auto-change width or height to match 16:9 ratio if aspect ratio check box is checked
+    $width.on('change', function() {
+      if ($ratio.prop('checked')) {
+        newWidth = Number($width.val());
+        newHeight = newWidth * (9 / 16);
+        $height.val(Math.round(newHeight));
+        checkbox_value = 'true';
+      } else {
+        checkbox_value = 'false';
       }
-
-    }).catch(function(err) {
-      // Error
-      alert('There was an error retrieving the Blank Space field information.');
     });
-  }
 
-  // Set saved width and height values
-  if (config.insert_width) {
-    $width.val(config.insert_width);
-  }
+    $height.on('change', function() {
+      if ($ratio.prop('checked')) {
+        newHeight = Number($height.val());
+        newWidth = newHeight * (16 / 9);
+        $width.val(Math.round(newWidth));
+        checkbox_value = 'true';
+      } else {
+        checkbox_value = 'false';
+      }
+    });
 
-  if (config.insert_height) {
-    $height.val(config.insert_height);
-  }
+    // Reset default dimensions when reset button is clicked
+    $reset.on('click', function() {
+      $width.val(560);
+      $height.val(315);
+    });
 
-  // Set saved checkbox value
-  if (config.checkbox_status === 'true') {
-    checkbox_boolean = true;
-  } else {
-    checkbox_boolean = false;
-  }
+    // Run setting functions
+    setDropDown();
 
-  $ratio.prop('checked', checkbox_boolean);
+    // Set input values when save button is clicked
+    $form.on('submit', function(e) {
+      var widthNum = Number($width.val());
+      var heightNum = Number($height.val());
+      var maxDimension = 99999;
+      var config = [];
 
-  // Auto-change width or height to match 16:9 ratio if aspect ratio check box is checked
-  $width.change(function() {
-    if ($ratio.prop('checked')) {
-      newWidth = Number($width.val());
-      newHeight = newWidth * (9 / 16);
-      $height.val(Math.round(newHeight));
-      checkbox_value = 'true';
-    } else {
-      checkbox_value = 'false';
-    }
-  });
+      config.link = $link.val();
+      config.space = $space.val();
+      config.width = $width.val();
+      config.height = $height.val();
+      config.checkbox = checkbox_value;
 
-  $height.change(function() {
-    if ($ratio.prop('checked')) {
-      newHeight = Number($height.val());
-      newWidth = newHeight * (16 / 9);
-      $width.val(Math.round(newWidth));
-      checkbox_value = 'true';
-    } else {
-      checkbox_value = 'false';
-    }
-  });
+      e.preventDefault();
 
-  // Reset default dimensions when reset button is clicked
-  $('#reset_dimensions').click(function() {
-    $width.val(560);
-    $height.val(315);
-  });
-
-  // Run setting functions
-  setLinkDropdown();
-  setSpaceDropdown();
-
-  // Go back a page when cancel button is clicked
-  $('#settings-cancel').click(function() {
-    history.back();
-  });
-
-  // Set input values when save button is clicked
-  $('#settings-save').click(function(e) {
-    var configSettings = {
-      select_link_field: $link.val(),
-      select_space_field: $space.val(),
-      insert_width: $width.val(),
-      insert_height: $height.val(),
-      checkbox_status: checkbox_value
-    };
-    var widthNum = Number($width.val());
-    var heightNum = Number($height.val());
-    var maxDimension = 99999;
-    e.preventDefault();
-
-    if ($link.val() === 'not selected' || $space.val() === 'not selected') {
-      // Check required fields are all filled
-      alert('Please fill in all required fields.');
-    } else if (!Number.isInteger(widthNum) || !Number.isInteger(heightNum) || widthNum > maxDimension || heightNum > maxDimension) {
-      // Check that width and height are full numbers less than 5000
-      alert('Please enter width and height as full numbers equal to or less than 99999px');
-    } else {
-      kintone.plugin.app.setConfig(configSettings, function() {
-        // Redirect to App Settings
-        alert('The plug-in settings have been saved. Please update the app!');
-        window.location.href = getSettingsUrl();
+      if (!Number.isInteger(widthNum) || !Number.isInteger(heightNum) || widthNum > maxDimension || heightNum > maxDimension) {
+        // Check that width and height are full numbers less than 5000
+        alert('Please enter width and height as full numbers equal to or less than 99999px');
+      } else {
+        kintone.plugin.app.setConfig(config, function() {
+          // Redirect to App Settings
+          alert('The plug-in settings have been saved. Please update the app!');
+          window.location.href = '/k/admin/app/flow?app=' + kintone.app.getId();
+        });
+      }
+      // Go back a page when cancel button is clicked
+      $cancelButton.on('click', function() {
+        window.location.href = '/k/admin/app/' + kintone.app.getId() + '/plugin/';
       });
-    }
+    });
   });
 })(jQuery, kintone.$PLUGIN_ID);
